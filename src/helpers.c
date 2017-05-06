273d659e51103a153dc8b1a32f333894d9c9498b
moon-watcher <langares@yahoo.es>
2017-05-06 03:36:54 +0200

    Two songs added

--- src/helpers.c
+++ src/helpers.c
@@ -16,11 +16,11 @@ static const Music music_list [ MUSIC_MAX ] =
     { MUSIC_NULL,     "Silence",     (u8*) NULL,            0 },
     { MUSIC_GAME,     "Game",        (u8*) music_game,     -1 },
     { MUSIC_DREAM,    "Dream",       (u8*) NULL,    -1 }, // music_dream
-    { MUSIC_CLEAR,    "Stage clear", (u8*) NULL,     0 }, // music_clear
+    { MUSIC_CLEAR,    "Stage clear", (u8*) music_clear,     0 },
     { MUSIC_NOBONUS,  "No bonus",    (u8*) NULL,   0 }, // music_nobonus
     { MUSIC_LEVEL,    "Level",       (u8*) music_level,     0 }, //
-    { MUSIC_GAMEOVER, "Game Over",   (u8*) NULL,  0 }, // music_gameover
-    { MUSIC_WELLDONE, "Welldone",    (u8*) NULL, -1 }  //
+    { MUSIC_GAMEOVER, "Game Over",   (u8*) music_gameover,  0 }, //
+    { MUSIC_WELLDONE, "Welldone",    (u8*) NULL, -1 }  // music_welldone
 };
 
 
